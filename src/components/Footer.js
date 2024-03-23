56d64b3d8bf799abf2ccfff327960debcf749132
SumitSouravChoudhury <95333079+SumitSouravChoudhury@users.noreply.github.com>
2024-03-23 14:40:56 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 
 function Footer() {
+
+  let year = new Date().getFullYear();
   return (
     <Container fluid  className="border-top mt-2 text-center bg-light text-dark py-3">
-        <p className="m-0">Resume Builder</p>
+        <p className="m-0">&copy; Copyright By Sumit Sourav Choudhury {year}</p>
     </Container>
   )
 }
